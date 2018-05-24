168ad7e7e1af17d629e7134b97d654d652e9d23a
gowufang <gowufang@gmail.com>
2018-05-25 03:30:53 +0800

    remove CamaraTest

--- app/src/main/java/me/wufang/facedetect/MainActivity.java
+++ app/src/main/java/me/wufang/facedetect/MainActivity.java
@@ -1,24 +1,10 @@
 package me.wufang.facedetect;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 import me.wufang.facedetect.net.DataRequest;
 
@@ -26,44 +12,13 @@ public class MainActivity extends AppCompatActivity {
 
     TextView response;//响应
 
-    //拍照
-    public static final int TAKE_PHOTO = 1;
-    private ImageView picture;
-    private Uri imageUri;
-    //拍照
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         response= (TextView) findViewById(R.id.response);
 
-        Button takePhoto = (Button) findViewById(R.id.take_photo);
-        picture = (ImageView) findViewById(R.id.picture);
 
-        takePhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-// 创建File对象， 用于存储拍照后的图片
-                File outputImage = new File(getExternalCacheDir(),
-                        "output_image.jpg");
-                try {
-                    if (outputImage.exists()) {
-                        outputImage.delete();
-                    }outputImage.createNewFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }if (Build.VERSION.SDK_INT >= 24) {
-                    imageUri = FileProvider.getUriForFile(MainActivity.this,
-                            "com.example.cameraalbumtest.fileprovider", outputImage);
-                } else {
-                    imageUri = Uri.fromFile(outputImage);
-                }
-                // 启动相机程序
-                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-                startActivityForResult(intent, TAKE_PHOTO);
-            }
-        });
 
         Handler myHandler=new Handler(){
             @Override
@@ -77,33 +32,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
-
         DataRequest request=new DataRequest(myHandler);
         request.execute();
-
-
-
-
-
-
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode){
-            case TAKE_PHOTO:
-                if (resultCode==RESULT_OK){
-                    try {
-                        Bitmap bitmap= BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                }
-                break;
-            default:
-                break;
-        }
-    }
-
-
 }
